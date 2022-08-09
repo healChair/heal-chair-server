5837c75b2d99b4d9ad541ee27c3222c9876a8750
tellang <tellang@daum.net>
2022-08-09 14:05:00 +0900

    [HC-6] heal chair 프로젝트 생성 초기 상태 커밋

---
+++ src/main/java/kr/healchairserver/HealchairApplication.java
@@ -0,0 +1,13 @@
+package kr.healchairserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealchairApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealchairApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/healchairserver/HealchairApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.healchairserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealchairApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
