f5593b932997ec584f70e917d18169ed452bb8f3
tellang <tellang@daum.net>
2022-08-10 15:06:40 +0900

    [HC-34] BaseRepository 구현
    추후 Repository에서 사용되는 공통적인 메소드를 위하여 JpaRepository를 상속하는 BaseRepository를 구현

---
+++ src/main/java/kr/healchairserver/repository/BaseRepository.java
@@ -0,0 +1,9 @@
+package kr.healchairserver.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.NoRepositoryBean;
+
+@NoRepositoryBean
+public interface BaseRepository<T, ID> extends JpaRepository<T, ID> {
+
+}
