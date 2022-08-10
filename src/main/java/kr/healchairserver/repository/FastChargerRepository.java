9d20a8372948d92988d8e7a2656b4773275149a7
tellang <tellang@daum.net>
2022-08-10 20:29:30 +0900

    [HC-10] FastCharger 레포지토리 구현

---
+++ src/main/java/kr/healchairserver/repository/FastChargerRepository.java
@@ -0,0 +1,9 @@
+package kr.healchairserver.repository;
+
+import kr.healchairserver.domain.model.FastCharger;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FastChargerRepository extends BaseRepository<FastCharger, Long> {
+
+}
