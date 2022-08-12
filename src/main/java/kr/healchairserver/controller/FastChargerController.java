2e9fbd55375a483883f623b97bf3f90de8a09245
tellang <tellang@daum.net>
2022-08-12 16:26:47 +0900

    누락된 FastChargerService search API 수정 추가
    FastChargerController에서도 `List` 가 아닌 `Page` 를 반환하도록 수정

--- src/main/java/kr/healchairserver/controller/FastChargerController.java
+++ src/main/java/kr/healchairserver/controller/FastChargerController.java
@@ -1,9 +1,9 @@
 package kr.healchairserver.controller;
 
 import static kr.healchairserver.domain.dto.FastChargerDto.Result;
-import java.util.List;
 import kr.healchairserver.service.FastChargerService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,7 +17,7 @@ public class FastChargerController {
     private final FastChargerService service;
 
     @GetMapping("/fastChargers")
-    public List<Result> search(
+    public Page<Result> search(
         Pageable pageable
     ) {
         return service.search(pageable);
